87ee50b6869ddf10189bff8a2eabd737e210bbaa
Matías Beraldo Fernández <matias.beraldo.work@gmail.com>
2023-09-12 21:52:42 +0200

    Added Mailchain SDK as typescript library

--- src/components/polygon/index.tsx
+++ src/components/polygon/index.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import { Mailchain } from '@mailchain/sdk';
-import { resolveAddress } from '@mailchain/sdk/internal';
+import { MailchainSDK } from '@/lib/mailchain';
 
 import { VmComponent } from '@/components/vm/VmComponent';
 
@@ -8,73 +7,13 @@ const Container = styled.div`
     height: 100vh;
 `;
 
-type Mail = {
-    context: Mailchain|null,
-    user: any,
-    connect: any,
-    disconnect: any,
-    send: any,
-    addressIsReachable: any,
-    getNearAddressFromAccount: any
-}
-
-const lib: Mail = {
-    context: null,
-    user: null,
-    connect: async (user: any) => {
-        const mailchain = Mailchain.fromSecretRecoveryPhrase(user.phrase);
-        const data = await mailchain.user();
-
-        if (data.address) {
-            lib.context = mailchain;
-            lib.user = data;
-        }
-
-        return data;
-    },
-    disconnect: () => {
-        lib.context = null;
-        lib.user = null;
-    },
-    send: async (to: string, subject: string, text: string) => {
-        const toMail = lib.getNearAddressFromAccount(to);
-
-        return lib.addressIsReachable(to).then(async (isReachable: boolean) => {
-            if (isReachable) {
-                const result = await lib.context?.sendMail({
-                    from: lib.user.address,
-                    to: [toMail],
-                    subject,
-                    content: {
-                        text,
-                        html: `<p>${text}</p>`,
-                    },
-                });
-    
-                return !result?.error;
-            } else {
-                return false;
-            }
-        })
-    },
-    addressIsReachable: async (account: string) => {
-        const address = lib.getNearAddressFromAccount(account);
-        const { data: resolvedAddress, error: resolveAddressError } = await resolveAddress(address);
-        return !resolveAddressError && "registered" === resolvedAddress?.type;
-    },
-    getNearAddressFromAccount: (account: string) => {
-        return `${account}@near.mailchain.com`;
-    }
-};
-
-
 export function PolygonZkEVM() {
     return (
         <Container>
             <VmComponent
                 src="mattb.near/widget/NearBox.Views.Home"
                 props={{
-                    MailChain: lib
+                    MailChain: new MailchainSDK()
                 }}
             />
         </Container>
---
+++ src/lib/mailchain.tsx
@@ -0,0 +1,71 @@
+import { Mailchain } from '@mailchain/sdk';
+import { resolveAddress } from '@mailchain/sdk/internal';
+
+interface UserInterface {
+  address: string;
+  username: string;
+}
+
+interface MailchainInterface {
+  context: Mailchain|null;
+  user: UserInterface|null;
+  connect: object;
+  disconnect: any;
+  send: any;
+  addressIsReachable: any;
+  getNearAddressFromAccount: any;
+}
+
+export class MailchainSDK implements MailchainInterface {
+  context: Mailchain|null = null;
+  user: UserInterface|null = null;
+
+  async connect(user: {mail: string, phrase: string}): Promise<UserInterface> {
+    const mailchain = Mailchain.fromSecretRecoveryPhrase(user.phrase);
+    const data: UserInterface = await mailchain.user();
+
+    if (data.address) {
+        this.context = mailchain;
+        this.user = data;
+    }
+
+    return data;
+  }
+
+  disconnect(): void {
+    this.context = null;
+    this.user = null;
+  }
+
+  async send(to: string, subject: string, text: string): Promise<boolean> {
+    const toMail = this.getNearAddressFromAccount(to);
+
+    return this.addressIsReachable(to).then(async (isReachable: boolean) => {
+        if (isReachable) {
+            const result = await this.context?.sendMail({
+                from: this.user!.address,
+                to: [toMail],
+                subject,
+                content: {
+                    text,
+                    html: `<p>${text}</p>`,
+                },
+            });
+
+            return !result?.error;
+        } else {
+            return false;
+        }
+    });
+  }
+
+  async addressIsReachable(account: string): Promise<boolean> {
+    const address = this.getNearAddressFromAccount(account);
+    const { data: resolvedAddress, error: resolveAddressError } = await resolveAddress(address);
+    return (!resolveAddressError && "registered" === resolvedAddress?.type);
+  }
+
+  getNearAddressFromAccount(account: string): string {
+    return `${account}@near.mailchain.com`;
+  }
+}
